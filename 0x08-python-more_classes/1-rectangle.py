f66752c1d6d07d402884d4dba95df3cb1fbebae8
Jose Cuervo <manuelmuoz1221@outlook.com>
2020-06-07 14:56:07 -0500

    [Project development]: 1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""class called rectangle"""
+
+
+class Rectangle:
+    """class called rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Method that initializes the instance"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """method that returns the value of the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """method that defines the width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """method that returns the value of the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """method that defines the height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
