9f9b35535f2edf06647ef2ed7638510d6aaf5af9
Jose Cuervo <manuelmuoz1221@outlook.com>
2020-07-27 13:37:36 -0500

    [Project development]: 1. My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+"""
+Class MyList that inherits from list
+"""
+
+
 class MyList(list):
-    """ Class that inherits the attributes references of class list
-
-    Args:
-        list: class list
-
-    """
+    """ Class that inherits the attributes"""
 
     def print_sorted(self):
         """ Method that prints the sorted list """
